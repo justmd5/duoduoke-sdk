445748ad11badb2996676f0230619a75beac4c2b
justmd5 <justmd5@163.com>
2018-09-26 13:18:57 +0000

    Apply fixes from StyleCI

--- tests/GoodsPromotionUrlGenerateTest.php
+++ tests/GoodsPromotionUrlGenerateTest.php
@@ -3,11 +3,10 @@
  * Created for duoduoke-sdk.
  * User: yc
  * Date: 2018/9/26
- * Time: 20:56
+ * Time: 20:56.
  */
 
 namespace Justmd5\DuoDuoKeTest;
-
 
 class GoodsPromotionUrlGenerateTest extends DdkTestCase
 {
@@ -18,9 +17,9 @@ class GoodsPromotionUrlGenerateTest extends DdkTestCase
     {
         $params = [
             'type'              => 'pdd.ddk.goods.promotion.url.generate',
-            'p_id' => $this->config['p_id'][0],
-            'generate_weapp'=>true,
-            'goods_id_list'   => [$this->config['goodsList'][0]],
+            'p_id'              => $this->config['p_id'][0],
+            'generate_weapp'    => true,
+            'goods_id_list'     => [$this->config['goodsList'][0]],
         ];
         $result = $this->app->request('pdd.ddk.goods.promotion.url.generate', $params);
 //        dd($result);
